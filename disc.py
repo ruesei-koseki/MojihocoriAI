a5aa391fb14d9cd86afc9ebef5d749f842ffc43d
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2022-11-05 00:24:10 +0900

    Add files via upload

--- disc.py
+++ disc.py
@@ -116,10 +116,9 @@ async def speak(result):
         restStep = 0
 
         if sanae.DATA.sa > 15 or sanae.DATA.rate == 0.0:
-            if "😅" not in sanae.DATA.lastSentenceInput:
-                for myname in sanae.DATA.settings["mynames"].split("|"):
-                    Message = Message.replace(myname, sanae.DATA.lastUserReplied)
-                sanae.MEMORY.addSentence(Message.replace(sanae.DATA.lastUserReplied, sanae.DATA.settings["myname"]), sanae.DATA.lastUserReplied)
+            for myname in sanae.DATA.settings["mynames"].split("|"):
+                Message = Message.replace(myname, sanae.DATA.lastUserReplied)
+            sanae.MEMORY.addSentence(Message.replace(sanae.DATA.lastUserReplied, sanae.DATA.settings["myname"]), sanae.DATA.lastUserReplied)
     
     prevTime = time.time()
     print("< ", sanae.DATA.sa)
