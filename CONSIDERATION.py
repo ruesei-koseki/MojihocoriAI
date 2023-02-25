1d6acd8fc5f9142b8471efdf4c6911cceb100349
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2023-02-25 22:28:12 +0900

    Add files via upload

--- CONSIDERATION.py
+++ CONSIDERATION.py
@@ -11,7 +11,7 @@ def looking(x, u, reply=True, force=False):
     try:
         #print("思考中...")
 
-        for kaisu in range(4):
+        for kaisu in range(5):
             if kaisu == 0:
                 rate = 1
             if kaisu == 1:
@@ -20,6 +20,8 @@ def looking(x, u, reply=True, force=False):
                 rate = 0.8
             if kaisu == 3:
                 rate = 0.7
+            if kaisu == 5:
+                rate = 0.6
             #今の気持ちから考える
             f = DATA.heart+1
             t = len(DATA.data["sentence"]) - 1
@@ -28,8 +30,8 @@ def looking(x, u, reply=True, force=False):
             for sen in DATA.data["sentence"][f:t]:
                 if i >= len(DATA.data["sentence"]) - 1:
                     break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
                     #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
                     if i+1>= len(DATA.data["sentence"]):
                         break
@@ -70,8 +72,8 @@ def looking(x, u, reply=True, force=False):
             for sen in DATA.data["sentence"][f:t]:
                 if i >= len(DATA.data["sentence"]) - 1:
                     break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
                     #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
                     if i+1>= len(DATA.data["sentence"]):
                         break
@@ -105,7 +107,7 @@ def looking(x, u, reply=True, force=False):
                 i += 1
                 ii += 1
 
-        for kaisu in range(4):
+        for kaisu in range(5):
             if kaisu == 0:
                 rate = 1
             if kaisu == 1:
@@ -114,6 +116,8 @@ def looking(x, u, reply=True, force=False):
                 rate = 0.8
             if kaisu == 3:
                 rate = 0.7
+            if kaisu == 5:
+                rate = 0.6
             #今の気持ちから考える
             f = DATA.heart+1
             t = len(DATA.data["sentence"]) - 1
@@ -122,8 +126,8 @@ def looking(x, u, reply=True, force=False):
             for sen in DATA.data["sentence"][f:t]:
                 if i >= len(DATA.data["sentence"]) - 1:
                     break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
                     #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
                     if i+1>= len(DATA.data["sentence"]):
                         break
@@ -164,8 +168,8 @@ def looking(x, u, reply=True, force=False):
             for sen in DATA.data["sentence"][f:t]:
                 if i >= len(DATA.data["sentence"]) - 1:
                     break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
                     #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
                     if i+1>= len(DATA.data["sentence"]):
                         break
@@ -175,100 +179,6 @@ def looking(x, u, reply=True, force=False):
                             if DATA.data["sentence"][i+1][1] == myname:
                                 isMine = True
                         if i != len(DATA.data["sentence"]) and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceHeart) < 0.75 and DATA.data["sentence"][i+1][1] != DATA.data["sentence"][i][1] and DATA.data["sentence"][i+1][1] != "!" and "!system" not in DATA.data["sentence"][i+1][1] and DATA.data["sentence"][i+1][0] != "☓" and DATA.data["sentence"][i+1][0] != "×" and DATA.data["sentence"][i+1][0] != "❌" and DATA.data["sentence"][i+1][0] != "⭕" and DATA.data["sentence"][i+1][0] != "○":
-                            #print("返信: {}, {}".format(DATA.data["sentence"][i+1][0], i+1))
-                            flag = True
-                            iii = 100
-                            for iiiii in range(4):
-                                if i+1+iiiii < len(DATA.data["sentence"]) - 1:
-                                    if DATA.data["sentence"][i+1+iiiii][0] == "☓" or DATA.data["sentence"][i+1+iiiii][0] == "×" or DATA.data["sentence"][i+1+iiiii][0] == "❌":
-                                        flag = False
-                                        iii = iiiii
-                                        break
-                            if flag:
-                                DATA.sa = ii
-                                DATA.heart = i+1
-                                DATA.heartLastSpeaker = DATA.data["sentence"][i+1][1]
-                                DATA.rate = rate
-                                return DATA.data["sentence"][i+1][0]
-                        else:
-                            pass
-                    else:
-                        DATA.heart = i
-                        DATA.rate = rate
-                        return
-                i += 1
-                ii += 1
-
-        for kaisu in range(4):
-            if kaisu == 0:
-                rate = 1
-            if kaisu == 1:
-                rate = 0.9
-            if kaisu == 2:
-                rate = 0.8
-            if kaisu == 3:
-                rate = 0.7
-            #今の気持ちから考える
-            f = DATA.heart+1
-            t = len(DATA.data["sentence"]) - 1
-            i = f
-            ii = 0
-            for sen in DATA.data["sentence"][f:t]:
-                if i >= len(DATA.data["sentence"]) - 1:
-                    break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
-                    #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
-                    if i+1>= len(DATA.data["sentence"]):
-                        break
-                    if reply:
-                        isMine = False
-                        for myname in DATA.settings["mynames"].split("|"):
-                            if DATA.data["sentence"][i+1][1] == myname:
-                                isMine = True
-                        if i != len(DATA.data["sentence"]) and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceHeart) < 0.75 and DATA.data["sentence"][i+1][1] != "!" and "!system" not in DATA.data["sentence"][i+1][1] and DATA.data["sentence"][i+1][0] != "☓" and DATA.data["sentence"][i+1][0] != "×" and DATA.data["sentence"][i+1][0] != "❌" and DATA.data["sentence"][i+1][0] != "⭕" and DATA.data["sentence"][i+1][0] != "○":
-                            #print("返信: {}, {}".format(DATA.data["sentence"][i+1][0], i+1))
-                            flag = True
-                            iii = 100
-                            for iiiii in range(4):
-                                if i+1+iiiii < len(DATA.data["sentence"]) - 1:
-                                    if DATA.data["sentence"][i+1+iiiii][0] == "☓" or DATA.data["sentence"][i+1+iiiii][0] == "×" or DATA.data["sentence"][i+1+iiiii][0] == "❌":
-                                        flag = False
-                                        iii = iiiii
-                                        break
-                            if flag:
-                                DATA.sa = ii
-                                DATA.heart = i+1
-                                DATA.heartLastSpeaker = DATA.data["sentence"][i+1][1]
-                                DATA.rate = rate
-                                return DATA.data["sentence"][i+1][0]
-                        else:
-                            pass
-                    else:
-                        DATA.heart = i
-                        DATA.rate = rate
-                        return
-                i += 1
-                ii += 1
-            #今の気持ちから考える
-            f = 0
-            t = DATA.heart-1
-            i = f
-            ii = 0
-            for sen in DATA.data["sentence"][f:t]:
-                if i >= len(DATA.data["sentence"]) - 1:
-                    break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
-                    #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
-                    if i+1>= len(DATA.data["sentence"]):
-                        break
-                    if reply:
-                        isMine = False
-                        for myname in DATA.settings["mynames"].split("|"):
-                            if DATA.data["sentence"][i+1][1] == myname:
-                                isMine = True
-                        if i != len(DATA.data["sentence"]) and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceHeart) < 0.75 and DATA.data["sentence"][i+1][1] != "!" and "!system" not in DATA.data["sentence"][i+1][1] and DATA.data["sentence"][i+1][0] != "☓" and DATA.data["sentence"][i+1][0] != "×" and DATA.data["sentence"][i+1][0] != "❌" and DATA.data["sentence"][i+1][0] != "⭕" and DATA.data["sentence"][i+1][0] != "○":
                             #print("返信: {}, {}".format(DATA.data["sentence"][i+1][0], i+1))
                             flag = True
                             iii = 100
@@ -302,7 +212,105 @@ def looking(x, u, reply=True, force=False):
                 rate = 0.8
             if kaisu == 3:
                 rate = 0.7
-            if kaisu == 4:
+            if kaisu == 5:
+                rate = 0.6
+            #今の気持ちから考える
+            f = DATA.heart+1
+            t = len(DATA.data["sentence"]) - 1
+            i = f
+            ii = 0
+            for sen in DATA.data["sentence"][f:t]:
+                if i >= len(DATA.data["sentence"]) - 1:
+                    break
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
+                    #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
+                    if i+1>= len(DATA.data["sentence"]):
+                        break
+                    if reply:
+                        isMine = False
+                        for myname in DATA.settings["mynames"].split("|"):
+                            if DATA.data["sentence"][i+1][1] == myname:
+                                isMine = True
+                        if i != len(DATA.data["sentence"]) and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceHeart) < 0.75 and DATA.data["sentence"][i+1][1] != "!" and "!system" not in DATA.data["sentence"][i+1][1] and DATA.data["sentence"][i+1][0] != "☓" and DATA.data["sentence"][i+1][0] != "×" and DATA.data["sentence"][i+1][0] != "❌" and DATA.data["sentence"][i+1][0] != "⭕" and DATA.data["sentence"][i+1][0] != "○":
+                            #print("返信: {}, {}".format(DATA.data["sentence"][i+1][0], i+1))
+                            flag = True
+                            iii = 100
+                            for iiiii in range(4):
+                                if i+1+iiiii < len(DATA.data["sentence"]) - 1:
+                                    if DATA.data["sentence"][i+1+iiiii][0] == "☓" or DATA.data["sentence"][i+1+iiiii][0] == "×" or DATA.data["sentence"][i+1+iiiii][0] == "❌":
+                                        flag = False
+                                        iii = iiiii
+                                        break
+                            if flag:
+                                DATA.sa = ii
+                                DATA.heart = i+1
+                                DATA.heartLastSpeaker = DATA.data["sentence"][i+1][1]
+                                DATA.rate = rate
+                                return DATA.data["sentence"][i+1][0]
+                        else:
+                            pass
+                    else:
+                        DATA.heart = i
+                        DATA.rate = rate
+                        return
+                i += 1
+                ii += 1
+            #今の気持ちから考える
+            f = 0
+            t = DATA.heart-1
+            i = f
+            ii = 0
+            for sen in DATA.data["sentence"][f:t]:
+                if i >= len(DATA.data["sentence"]) - 1:
+                    break
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
+                    #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
+                    if i+1>= len(DATA.data["sentence"]):
+                        break
+                    if reply:
+                        isMine = False
+                        for myname in DATA.settings["mynames"].split("|"):
+                            if DATA.data["sentence"][i+1][1] == myname:
+                                isMine = True
+                        if i != len(DATA.data["sentence"]) and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.75 and Levenshtein.ratio(DATA.data["sentence"][i+1][0], DATA.lastSentenceHeart) < 0.75 and DATA.data["sentence"][i+1][1] != "!" and "!system" not in DATA.data["sentence"][i+1][1] and DATA.data["sentence"][i+1][0] != "☓" and DATA.data["sentence"][i+1][0] != "×" and DATA.data["sentence"][i+1][0] != "❌" and DATA.data["sentence"][i+1][0] != "⭕" and DATA.data["sentence"][i+1][0] != "○":
+                            #print("返信: {}, {}".format(DATA.data["sentence"][i+1][0], i+1))
+                            flag = True
+                            iii = 100
+                            for iiiii in range(4):
+                                if i+1+iiiii < len(DATA.data["sentence"]) - 1:
+                                    if DATA.data["sentence"][i+1+iiiii][0] == "☓" or DATA.data["sentence"][i+1+iiiii][0] == "×" or DATA.data["sentence"][i+1+iiiii][0] == "❌":
+                                        flag = False
+                                        iii = iiiii
+                                        break
+                            if flag:
+                                DATA.sa = ii
+                                DATA.heart = i+1
+                                DATA.heartLastSpeaker = DATA.data["sentence"][i+1][1]
+                                DATA.rate = rate
+                                return DATA.data["sentence"][i+1][0]
+                        else:
+                            pass
+                    else:
+                        DATA.heart = i
+                        DATA.rate = rate
+                        return
+                i += 1
+                ii += 1
+
+        for kaisu in range(6):
+            if kaisu == 0:
+                rate = 1
+            if kaisu == 1:
+                rate = 0.9
+            if kaisu == 2:
+                rate = 0.8
+            if kaisu == 3:
+                rate = 0.7
+            if kaisu == 5:
+                rate = 0.6
+            if kaisu == 6:
                 rate = 0.0
             #今の気持ちから考える
             f = DATA.heart+1
@@ -312,8 +320,8 @@ def looking(x, u, reply=True, force=False):
             for sen in DATA.data["sentence"][f:t]:
                 if i >= len(DATA.data["sentence"]) - 1:
                     break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
                     #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
                     if i+1>= len(DATA.data["sentence"]):
                         break
@@ -354,8 +362,8 @@ def looking(x, u, reply=True, force=False):
             for sen in DATA.data["sentence"][f:t]:
                 if i >= len(DATA.data["sentence"]) - 1:
                     break
-                into = x
-                if Levenshtein.ratio(into,  DATA.data["sentence"][i][0]) >= rate:
+                into = "{}: {}".format(u, x)
+                if Levenshtein.ratio(into,  "{}: {}".formt(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
                     #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
                     if i+1>= len(DATA.data["sentence"]):
                         break
