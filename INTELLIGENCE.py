8d2a78b1f53548d240d57cb3ada569ae6ca522af
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2023-03-06 15:38:14 +0900

    Add files via upload

--- INTELLIGENCE.py
+++ INTELLIGENCE.py
@@ -18,5 +18,6 @@ def replaceWords(x1, x2):
         if afterWord[0] in x1:
             for beforeWord in random.sample(DATA.data["words"], len(DATA.data["words"])):
                 if beforeWord[0] in x2:
-                    result = result.replace(beforeWord[0], afterWord[0])
+                    if x1 != x2:
+                        result = result.replace(beforeWord[0], afterWord[0])
     return result
