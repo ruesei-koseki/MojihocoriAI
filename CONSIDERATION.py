992d934cd5ad537012744475732cbeada8b8c843
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2023-03-31 21:07:26 +0900

    Add files via upload

--- CONSIDERATION.py
+++ CONSIDERATION.py
@@ -40,6 +40,112 @@ def looking(x, u, reply=True, force=False):
             for sen in DATA.data["sentence"][f:t]:
                 if i >= len(DATA.data["sentence"]) - 1:
                     break
+                into = x
+                if Levenshtein.normalized_similarity(into, DATA.data["sentence"][i][0]) >= rate:
+                    #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
+                    if i+1>= len(DATA.data["sentence"]):
+                        break
+                    if reply:
+                        isMine = False
+                        for myname in DATA.settings["mynames"].split("|"):
+                            if DATA.data["sentence"][i+1][1] == myname:
+                                isMine = True
+                        if i != len(DATA.data["sentence"]) and Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.75 and Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.75 and Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceHeart) < 0.75 and DATA.data["sentence"][i+1][1] != DATA.data["sentence"][i][1] and DATA.data["sentence"][i+1][1] != "!" and "!system" not in DATA.data["sentence"][i+1][1]:
+                            print("返信: {}, {}".format(DATA.data["sentence"][i+1][0], i+1))
+                            flag = True
+                            for iiiii in range(4):
+                                if i+1+iiiii < len(DATA.data["sentence"]) - 1:
+                                    if DATA.data["sentence"][i+1+iiiii][0] == "☓" or DATA.data["sentence"][i+1+iiiii][0] == "×" or DATA.data["sentence"][i+1+iiiii][0] == "❌":
+                                        flag = False
+                                        if iiiii == 0:
+                                            return "×"
+                                        break
+                            if flag:
+                                DATA.sa = ii
+                                DATA.heart = i+1
+                                DATA.heartLastSpeaker = DATA.data["sentence"][i+1][1]
+                                DATA.rate = rate
+                                return DATA.data["sentence"][i+1][0]
+                        else:
+                            pass
+                    else:
+                        DATA.heart = i
+                        DATA.rate = rate
+                        return
+                i += 1
+                ii += 1
+            #今の気持ちから考える
+            f = 0
+            t = DATA.heart-1
+            i = f
+            ii = 0
+            for sen in DATA.data["sentence"][f:t]:
+                if i >= len(DATA.data["sentence"]) - 1:
+                    break
+                into = x
+                if Levenshtein.normalized_similarity(into, DATA.data["sentence"][i][0]) >= rate:
+                    #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
+                    if i+1>= len(DATA.data["sentence"]):
+                        break
+                    if reply:
+                        isMine = False
+                        for myname in DATA.settings["mynames"].split("|"):
+                            if DATA.data["sentence"][i+1][1] == myname:
+                                isMine = True
+                        if i != len(DATA.data["sentence"]) and Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.75 and Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.75 and Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceHeart) < 0.75 and DATA.data["sentence"][i+1][1] != DATA.data["sentence"][i][1] and DATA.data["sentence"][i+1][1] != "!" and "!system" not in DATA.data["sentence"][i+1][1]:
+                            print("返信: {}, {}".format(DATA.data["sentence"][i+1][0], i+1))
+                            flag = True
+                            for iiiii in range(4):
+                                if i+1+iiiii < len(DATA.data["sentence"]) - 1:
+                                    if DATA.data["sentence"][i+1+iiiii][0] == "☓" or DATA.data["sentence"][i+1+iiiii][0] == "×" or DATA.data["sentence"][i+1+iiiii][0] == "❌":
+                                        flag = False
+                                        if iiiii == 0:
+                                            return "×"
+                                        break
+                            if flag:
+                                DATA.sa = ii
+                                DATA.heart = i+1
+                                DATA.heartLastSpeaker = DATA.data["sentence"][i+1][1]
+                                DATA.rate = rate
+                                return DATA.data["sentence"][i+1][0]
+                        else:
+                            pass
+                    else:
+                        DATA.heart = i
+                        DATA.rate = rate
+                        return
+                i += 1
+                ii += 1
+
+        for kaisu in range(10):
+            if kaisu == 0:
+                rate = 1
+            if kaisu == 1:
+                rate = 0.9
+            if kaisu == 2:
+                rate = 0.8
+            if kaisu == 3:
+                rate = 0.7
+            if kaisu == 4:
+                rate = 0.6
+            if kaisu == 5:
+                rate = 0.5
+            if kaisu == 6:
+                rate = 0.4
+            if kaisu == 7:
+                rate = 0.3
+            if kaisu == 8:
+                rate = 0.2
+            if kaisu == 9:
+                rate = 0.1
+            #今の気持ちから考える
+            f = DATA.heart+1
+            t = len(DATA.data["sentence"]) - 1
+            i = f
+            ii = 0
+            for sen in DATA.data["sentence"][f:t]:
+                if i >= len(DATA.data["sentence"]) - 1:
+                    break
                 into = "{}: {}".format(u, x)
                 if Levenshtein.normalized_similarity(into, "{}: {}".format(DATA.data["sentence"][i][1], DATA.data["sentence"][i][0])) >= rate:
                     #print("類似: {}, {}".format(DATA.data["sentence"][i][0], i))
