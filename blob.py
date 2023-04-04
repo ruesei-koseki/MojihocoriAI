5bdb36cfb591de4ba235a06c282b52a0c1715bc1
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2023-04-04 21:34:30 +0900

    Add files via upload

--- blob.py
+++ blob.py
@@ -118,15 +118,17 @@ def receive(x, u, add=True, force=False):
             DATA.lastUser = u
         DATA.userLog.append(u)
         DATA.userLog.pop(0)
+        result = ""
         if add:
             if x.count("\n") >= 1:
                 for xx in x.split("\n"):
                     MEMORY.learnSentence(xx, u)
+                    result += CONSIDERATION.looking(xx, u, force=force) + "\n"
             else:
                 MEMORY.learnSentence(x, u)
+                result += CONSIDERATION.looking(x, u, force=force)
         if x == "×" or x == "☓" or x == "❌":
             DATA.data["sentence"].insert(DATA.heart+1, ["×", "!"])
-        result = CONSIDERATION.looking(x, u, force=force)
         if result == None:
             DATA.myVoice = None
             return
