4df21ee6ac326c78b7c71cbdd015dd987e0b88de
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2026-06-22 22:01:02 +0900

    Add files via upload

--- CONSIDERATION.py
+++ CONSIDERATION.py
@@ -13,10 +13,10 @@ def looking(x, u, reply=True, force=False):
         t = len(DATA.data["sentence"]) - 1
         i = f
         d = 0
-        b = 0
+        b = None
         for sen in DATA.data["sentence"][f:t]:
             c = Levenshtein.normalized_similarity(x, sen[0]) 
-            if d > c:
+            if d < c:
                 if (i != len(DATA.data["sentence"]) and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.85 and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.85 and
@@ -35,7 +35,7 @@ def looking(x, u, reply=True, force=False):
         i = f
         for sen in DATA.data["sentence"][f:t]:
             c = Levenshtein.normalized_similarity(x, sen[0]) 
-            if d > c:
+            if d < c:
                 if (i != len(DATA.data["sentence"]) and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.85 and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.85 and
@@ -49,26 +49,27 @@ def looking(x, u, reply=True, force=False):
                     d = c
                     b = i
             i += 1
-        flag = False
-        for iiiii in range(1):
-            if b+2+iiiii < len(DATA.data["sentence"]) - 1:
-                if DATA.data["sentence"][b+2+iiiii][0] == "!good":
-                    flag = True
-                    break
-        for iiiii in range(1):
-            if b+2+iiiii < len(DATA.data["sentence"]) - 1:
-                if DATA.data["sentence"][b+2+iiiii][0] == "!bad":
-                    flag = False
-                    break
-        if flag and b and d >= 0.6:
-            print("類似: {}, {}, {}".format(DATA.data["sentence"][b][0], b, d))
-            print("返信: {}, {}".format(DATA.data["sentence"][b+1][0], b+1))
-            DATA.lastSentenceInputHeart = DATA.data["sentence"][b][0]
-            DATA.heartLastSpeakerInput = DATA.data["sentence"][b][1]
-            DATA.heart = b+1
-            DATA.heartLastSpeaker = DATA.data["sentence"][b+1][1]
-            DATA.lastSentenceHeart = DATA.data["sentence"][b+1][0]
-            return DATA.data["sentence"][b+1][0]
+        if b:
+            flag = False
+            for iiiii in range(1):
+                if b+2+iiiii < len(DATA.data["sentence"]) - 1:
+                    if DATA.data["sentence"][b+2+iiiii][0] == "!good":
+                        flag = True
+                        break
+            for iiiii in range(1):
+                if b+2+iiiii < len(DATA.data["sentence"]) - 1:
+                    if DATA.data["sentence"][b+2+iiiii][0] == "!bad":
+                        flag = False
+                        break
+            if flag and d >= 0.6:
+                print("類似: {}, {}, {}".format(DATA.data["sentence"][b][0], b, d))
+                print("返信: {}, {}".format(DATA.data["sentence"][b+1][0], b+1))
+                DATA.lastSentenceInputHeart = DATA.data["sentence"][b][0]
+                DATA.heartLastSpeakerInput = DATA.data["sentence"][b][1]
+                DATA.heart = b+1
+                DATA.heartLastSpeaker = DATA.data["sentence"][b+1][1]
+                DATA.lastSentenceHeart = DATA.data["sentence"][b+1][0]
+                return DATA.data["sentence"][b+1][0]
 
 
 
@@ -77,10 +78,10 @@ def looking(x, u, reply=True, force=False):
         t = len(DATA.data["sentence"]) - 1
         i = f
         d = 0
-        b = 0
+        b = None
         for sen in DATA.data["sentence"][f:t]:
             c = Levenshtein.normalized_similarity(u+": "+x, sen[1]+": "+sen[0]) 
-            if d > c:
+            if d < c:
                 if (i != len(DATA.data["sentence"]) and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.85 and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.85 and
@@ -99,7 +100,7 @@ def looking(x, u, reply=True, force=False):
         i = f
         for sen in DATA.data["sentence"][f:t]:
             c = Levenshtein.normalized_similarity(u+": "+x, sen[1]+": "+sen[0]) 
-            if d > c:
+            if d < c:
                 if (i != len(DATA.data["sentence"]) and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentenceInput) < 0.85 and
                     Levenshtein.normalized_similarity(DATA.data["sentence"][i+1][0], DATA.lastSentence) < 0.85 and
@@ -113,21 +114,22 @@ def looking(x, u, reply=True, force=False):
                     d = c
                     b = i
             i += 1
-        flag = True
-        for iiiii in range(1):
-            if b+2+iiiii < len(DATA.data["sentence"]) - 1:
-                if DATA.data["sentence"][b+2+iiiii][0] == "!bad":
-                    flag = False
-                    break
-        if flag and b:
-            print("類似: {}, {}, {}".format(DATA.data["sentence"][b][0], b, d))
-            print("返信: {}, {}".format(DATA.data["sentence"][b+1][0], b+1))
-            DATA.lastSentenceInputHeart = DATA.data["sentence"][b][0]
-            DATA.heartLastSpeakerInput = DATA.data["sentence"][b][1]
-            DATA.heart = b+1
-            DATA.heartLastSpeaker = DATA.data["sentence"][b+1][1]
-            DATA.lastSentenceHeart = DATA.data["sentence"][b+1][0]
-            return DATA.data["sentence"][b+1][0]
+        if b:
+            flag = True
+            for iiiii in range(1):
+                if b+2+iiiii < len(DATA.data["sentence"]) - 1:
+                    if DATA.data["sentence"][b+2+iiiii][0] == "!bad":
+                        flag = False
+                        break
+            if flag:
+                print("類似: {}, {}, {}".format(DATA.data["sentence"][b][0], b, d))
+                print("返信: {}, {}".format(DATA.data["sentence"][b+1][0], b+1))
+                DATA.lastSentenceInputHeart = DATA.data["sentence"][b][0]
+                DATA.heartLastSpeakerInput = DATA.data["sentence"][b][1]
+                DATA.heart = b+1
+                DATA.heartLastSpeaker = DATA.data["sentence"][b+1][1]
+                DATA.lastSentenceHeart = DATA.data["sentence"][b+1][0]
+                return DATA.data["sentence"][b+1][0]
 
 
         #今の気持ちから考える
@@ -167,21 +169,22 @@ def looking(x, u, reply=True, force=False):
                     d = c
                     b = i
             i += 1
-        flag = True
-        for iiiii in range(1):
-            if b+2+iiiii < len(DATA.data["sentence"]) - 1:
-                if DATA.data["sentence"][b+2+iiiii][0] == "!bad":
-                    flag = False
-                    break
-        if flag and b:
-            print("類似: {}, {}, {}".format(DATA.data["sentence"][b][0], b, d))
-            print("返信: {}, {}".format(DATA.data["sentence"][b+1][0], b+1))
-            DATA.lastSentenceInputHeart = DATA.data["sentence"][b][0]
-            DATA.heartLastSpeakerInput = DATA.data["sentence"][b][1]
-            DATA.heart = b+1
-            DATA.heartLastSpeaker = DATA.data["sentence"][b+1][1]
-            DATA.lastSentenceHeart = DATA.data["sentence"][b+1][0]
-            return DATA.data["sentence"][b+1][0]
+        if b:
+            flag = True
+            for iiiii in range(1):
+                if b+2+iiiii < len(DATA.data["sentence"]) - 1:
+                    if DATA.data["sentence"][b+2+iiiii][0] == "!bad":
+                        flag = False
+                        break
+            if flag:
+                print("類似: {}, {}, {}".format(DATA.data["sentence"][b][0], b, d))
+                print("返信: {}, {}".format(DATA.data["sentence"][b+1][0], b+1))
+                DATA.lastSentenceInputHeart = DATA.data["sentence"][b][0]
+                DATA.heartLastSpeakerInput = DATA.data["sentence"][b][1]
+                DATA.heart = b+1
+                DATA.heartLastSpeaker = DATA.data["sentence"][b+1][1]
+                DATA.lastSentenceHeart = DATA.data["sentence"][b+1][0]
+                return DATA.data["sentence"][b+1][0]
 
 
     except:
